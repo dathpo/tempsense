81690acda111b10a6a0eb877d465427f40976c9d
David T. Pocock <davidtpocock@gmail.com>
2024-10-04 20:49:06 +0100

    Remove BAS, format code and use logging rather than printk

--- src/hts.c
+++ src/hts.c
@@ -9,21 +9,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
 #include <errno.h>
-
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/byteorder.h>
-
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+
+LOG_MODULE_REGISTER(hts, LOG_LEVEL_INF);
 
 static const struct device *temp_dev = DEVICE_DT_GET_ANY(nordic_nrf_temp);
 
@@ -31,45 +32,37 @@ static uint8_t simulate_htm;
 static uint8_t indicating;
 static struct bt_gatt_indicate_params ind_params;
 
-static void htmc_ccc_cfg_changed(const struct bt_gatt_attr *attr,
-								 uint16_t value)
+static void htmc_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
 {
 	simulate_htm = (value == BT_GATT_CCC_INDICATE) ? 1 : 0;
 }
 
-static void indicate_cb(struct bt_conn *conn,
-						struct bt_gatt_indicate_params *params, uint8_t err)
+static void indicate_cb(struct bt_conn *conn, struct bt_gatt_indicate_params *params, uint8_t err)
 {
-	printk("Indication %s\n", err != 0U ? "fail" : "success");
+	LOG_INF("Indication %s", err != 0U ? "fail" : "success");
 }
 
 static void indicate_destroy(struct bt_gatt_indicate_params *params)
 {
-	printk("Indication complete\n");
+	LOG_INF("Indication complete");
 	indicating = 0U;
 }
 
 /* Health Thermometer Service Declaration */
-BT_GATT_SERVICE_DEFINE(hts_svc,
-					   BT_GATT_PRIMARY_SERVICE(BT_UUID_HTS),
+BT_GATT_SERVICE_DEFINE(hts_svc, BT_GATT_PRIMARY_SERVICE(BT_UUID_HTS),
 					   BT_GATT_CHARACTERISTIC(BT_UUID_HTS_MEASUREMENT, BT_GATT_CHRC_INDICATE,
 											  BT_GATT_PERM_NONE, NULL, NULL, NULL),
-					   BT_GATT_CCC(htmc_ccc_cfg_changed,
-								   BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+					   BT_GATT_CCC(htmc_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 					   /* more optional Characteristics */
 );
 
 void hts_init(void)
 {
-	if (temp_dev == NULL || !device_is_ready(temp_dev))
-	{
-		printk("no temperature device; using simulated data\n");
+	if (temp_dev == NULL || !device_is_ready(temp_dev)) {
+		LOG_INF("no temperature device; using simulated data");
 		temp_dev = NULL;
-	}
-	else
-	{
-		printk("temp device is %p, name is %s\n", temp_dev,
-			   temp_dev->name);
+	} else {
+		LOG_INF("temp device is %p, name is %s", temp_dev, temp_dev->name);
 	}
 }
 
@@ -78,24 +71,20 @@ void hts_indicate(void)
 	/* Temperature measurements simulation */
 	struct sensor_value temp_value;
 
-	if (simulate_htm)
-	{
+	if (simulate_htm) {
 		static uint8_t htm[5];
 		static double temperature = 20U;
 		uint32_t mantissa;
 		uint8_t exponent;
 		int r;
 
-		if (indicating)
-		{
+		if (indicating) {
 			return;
 		}
 
-		if (!temp_dev)
-		{
+		if (!temp_dev) {
 			temperature++;
-			if (temperature == 30U)
-			{
+			if (temperature == 30U) {
 				temperature = 20U;
 			}
 
@@ -103,22 +92,19 @@ void hts_indicate(void)
 		}
 
 		r = sensor_sample_fetch(temp_dev);
-		if (r)
-		{
-			printk("sensor_sample_fetch failed return: %d\n", r);
+		if (r) {
+			LOG_INF("sensor_sample_fetch failed return: %d", r);
 		}
 
-		r = sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP,
-							   &temp_value);
-		if (r)
-		{
-			printk("sensor_channel_get failed return: %d\n", r);
+		r = sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP, &temp_value);
+		if (r) {
+			LOG_INF("sensor_channel_get failed return: %d", r);
 		}
 
 		temperature = sensor_value_to_double(&temp_value);
 
 	gatt_indicate:
-		printf("temperature is %gC\n", temperature);
+		LOG_INF("temperature is %gC", temperature);
 
 		mantissa = (uint32_t)(temperature * 100);
 		exponent = (uint8_t)-2;
@@ -133,8 +119,7 @@ void hts_indicate(void)
 		ind_params.data = &htm;
 		ind_params.len = sizeof(htm);
 
-		if (bt_gatt_indicate(NULL, &ind_params) == 0)
-		{
+		if (bt_gatt_indicate(NULL, &ind_params) == 0) {
 			indicating = 1U;
 		}
 	}
